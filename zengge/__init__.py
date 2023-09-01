8bb9373f21c855c993878aee77409bcae72f4269
SleepyNinja <hillbilly.tiger@gmail.com>
2023-08-31 21:12:23 -0400

    1st attempt port to bleak

--- zengge/__init__.py
+++ zengge/__init__.py
@@ -1,15 +1,19 @@
 # Python module for control of Zengge bluetooth LED bulbs
 #
 # Copyright 2016 Matthew Garrett <mjg59@srcf.ucam.org>
+# This code was modified by SilentNinja0o
+# It is being ported from the bluepy library to bleak
 #
 # This code is released under the terms of the MIT license. See the LICENSE
 # file for more details.
 
 import time
 
-from bluepy import btle
+#from bluepy import btle    #from bluepy lib
+from bleak import BleakClient, BleakScanner
+from bleak.exc import BleakError
 
-class Delegate(btle.DefaultDelegate):
+'''class Delegate(btle.DefaultDelegate):
     def __init__(self, bulb):
       self.bulb = bulb
       btle.DefaultDelegate.__init__(self)
@@ -19,11 +23,27 @@ class Delegate(btle.DefaultDelegate):
         power = True
       else:
         power = False
-      self.bulb.set_state(data[9], data[6], data[7], data[8], power)
+      self.bulb.set_state(data[9], data[6], data[7], data[8], power)'''
 
 class zengge:
   def __init__(self, mac):
     self.mac = mac
+    self.client = None
+    self.device = None
+    self.notifyhandle = ""  #handle NOTIFY
+    self.statehandle = ""   #handle ffe4
+    self.redhandle = ""     #handle ffe6
+    self.greenhandle = ""   #handle ffe7
+    self.bluehandle = ""    #handle ffe8
+    self.rgbwhandle = ""    #handle ffe9
+    self.whitehandle = ""   #handle ffea
+
+  def handleNotification(self, cHandle, data):
+    if data[5] > 2:
+      power = True
+    else:
+      power = False
+    self.set_state(data[9], data[6], data[7], data[8], power)
 
   def set_state(self, white, red, green, blue, power):
     self.white = white
@@ -32,76 +52,69 @@ class zengge:
     self.blue = blue
     self.power = power
 
-  def connect(self):
-    self.device = btle.Peripheral(self.mac, addrType=btle.ADDR_TYPE_PUBLIC)
-    self.device.setDelegate(Delegate(self))
+  async def connect(self):
+    #self.device = btle.Peripheral(self.mac, addrType=btle.ADDR_TYPE_PUBLIC)    #from bluepy lib
+    #self.device.setDelegate(Delegate(self))    #from bluepy lib
 
-    handles = self.device.getCharacteristics()
-    for handle in handles:
-      if handle.uuid == "ffe4":
-        self.statehandle = handle
-      if handle.uuid == "ffe6":
-        self.redhandle = handle
-      if handle.uuid == "ffe7":
-        self.greenhandle = handle
-      if handle.uuid == "ffe8":
-        self.bluehandle = handle
-      if handle.uuid == "ffe9":
-        self.rgbwhandle = handle
-      if handle.uuid == "ffea":
-        self.whitehandle = handle
+    self.device = await BleakScanner.find_device_by_address(self.mac, timeout=10.0)
+    if not self.device:
+        raise BleakError(f"A device with address {self.mac} could not be found.")
+    self.client = BleakClient(self.device)
+    await self.client.connect()
+    await self.client.start_notify(self.notifyhandle, self.handleNotification)
 
     self.get_state()
 
-  def send_packet(self, handle, data):
+  async def send_packet(self, uuid, data):
     initial = time.time()
     while True:
       if time.time() - initial >= 10:
         return False
       try:
-        return handle.write(bytes(data), withResponse=True)
+        #return handle.write(bytes(data), withResponse=True)    #from bluepy lib
+        return await self.client.write_gatt_char(uuid, data, response=True)
       except:
-        self.connect()
+        await self.connect()
 
-  def off(self):
+  async def off(self):
     self.power = False
     packet = bytearray([0xcc, 0x24, 0x33])
-    self.send_packet(self.rgbwhandle, packet)
+    await self.send_packet(self.rgbwhandle, packet)
 
-  def on(self):
+  async def on(self):
     self.power = True
     packet = bytearray([0xcc, 0x23, 0x33])
-    self.send_packet(self.rgbwhandle, packet)
+    await self.send_packet(self.rgbwhandle, packet)
 
-  def set_rgb(self, red, green, blue):
+  async def set_rgb(self, red, green, blue):
     self.red = red
     self.green = green
     self.blue = blue
     self.white = 0
     packet = bytearray([0x56, red, green, blue, 0x00, 0xf0, 0xaa])
-    self.send_packet(self.rgbwhandle, packet)
+    await self.send_packet(self.rgbwhandle, packet)
 
-  def set_white(self, white):
+  async def set_white(self, white):
     self.red = 0
     self.green = 0
     self.blue = 0
     self.white = white
     packet = bytearray([0x56, 0x00, 0x00, 0x00, white, 0x0f, 0xaa])
-    self.send_packet(self.rgbwhandle, packet)
+    await self.send_packet(self.rgbwhandle, packet)
 
-  def set_rgbw(self, red, green, blue, white):
+  async def set_rgbw(self, red, green, blue, white):
     self.red = red
     self.green = green
     self.blue = blue
     self.white = white
-    self.send_packet(self.redhandle, bytearray([red]))
-    self.send_packet(self.greenhandle, bytearray([green]))
-    self.send_packet(self.bluehandle, bytearray([blue]))
-    self.send_packet(self.whitehandle, bytearray([white]))
+    await self.send_packet(self.redhandle, bytearray([red]))
+    await self.send_packet(self.greenhandle, bytearray([green]))
+    await self.send_packet(self.bluehandle, bytearray([blue]))
+    await self.send_packet(self.whitehandle, bytearray([white]))
 
-  def get_state(self):
-    self.send_packet(self.rgbwhandle, bytearray([0xef, 0x01, 0x77]))
-    self.device.waitForNotifications(1.0)
+  async def get_state(self):
+    await self.send_packet(self.rgbwhandle, bytearray([0xef, 0x01, 0x77]))
+    #self.client.waitForNotifications(1.0)    #from bluepy lib
 
   def get_on(self):
     return self.power
